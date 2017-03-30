1a291cb16e47f5f593b77a57005aeff480898527
Leu <alexacaalves@gmail.com>
2017-03-29 21:11:44 -0300

    feat LinearList.java: added methods

--- linearListOnArray/src/linearlistonarray/LinearList.java
+++ linearListOnArray/src/linearlistonarray/LinearList.java
@@ -1,59 +1,150 @@
 package linearlistonarray;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Leu <@leunardosevla at twitter.com>
  */
 public class LinearList {
+
     private int startList;
     private int endList;
-    private int lengthList;
-    private int highList;
-    private int lowList;
-    
-    public LinearList () {
-        
-    } 
 
-    public int getStartList() {
-        return startList;
+    public LinearList() {
     }
 
-    public void setStartList(int startList) {
-        this.startList = startList;
+    public LinearList(Integer array[], int nodes) {
+        startList = (array.length - nodes) / 2 + 1;
+        endList = startList + nodes;
     }
 
-    public int getEndList() {
-        return endList;
+    public void createLinearList(Integer array[]) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1;
+        }
+
+        for (int i = startList; i < endList; i++) {
+            array[i] = null;
+        }
     }
 
-    public void setEndList(int endList) {
-        this.endList = endList;
+    public void addFirstNode(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            if (startList != 0) {
+                startList--;
+                array[startList] = null;
+            } else {
+                System.out.println("Não é possível adicionar mais nodos ao início.");
+            }
+        } else {
+            System.out.println("Não há lista linear.");
+        }
     }
 
-    public int getLengthList() {
-        return lengthList;
+    public void addMiddleNode(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            int length = endList - startList;
+            int middle = length / 2 + 1;
+            //verifica se tem espaço a esquerda na LA
+            if (startList > 0) { 
+                //puxa todos os elementos de LLinicio até LLmeio para um slot à esquerda
+                for (int i = startList - 1; i < middle; i++) {                     
+                    array[i] = null;
+                }
+                //adiciona nodo no meio
+                array[middle] = null;
+                //coloca os nodos no lugar do meio até o final da LA
+                for (int i = middle + 1; i < endList; i++) {
+                    array[i] = null;
+                }
+                startList--;
+            //verifica se tem espaço a direita na LA    
+            } else if (endList <= array.length) {
+                //coloca os nodos no lugar até MEIO
+                for (int i = startList; i < middle; i++) {
+                    array[i] = null;
+                }
+                //adiciona o nodo no MEIO
+                array[middle] = null;
+                //empurra o restante para o FINAL
+                for (int i = middle + 1; i < endList; i++) {
+                    array[i] = null;
+                }
+                endList++;
+            } else {
+                System.out.println("Não é possível adicionar mais nodos.");
+            }
+        }
     }
 
-    public void setLengthList(int lengthList) {
-        this.lengthList = lengthList;
+    public void addLastNode(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            if (endList <= array.length) {
+                array[endList] = null;
+                endList++;
+            } else {
+                System.out.println("Não há mais espaço no final.");
+            }
+        } else {
+            System.out.println("Não há lista linear.");
+        }
     }
 
-    public int getHighList() {
-        return highList;
+    public void removeFirstNode(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            array[startList] = 1;
+            startList++;
+            System.out.println("Nodo removido.");
+        }
     }
 
-    public void setHighList(int highList) {
-        this.highList = highList;
+    public void removeEndNode(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            array[endList] = 1;
+            endList--;
+            System.out.println("Nodo removido.");
+        }
     }
 
-    public int getLowList() {
-        return lowList;
+    public void removeLinearList(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            for (int i = startList; i < endList; i++) {
+                array[i] = 1;
+            }
+        } else {
+            System.out.println("Não há lista linear.");
+        }
     }
 
-    public void setLowList(int lowList) {
-        this.lowList = lowList;
+    public void showLinearList(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            Integer array2[] = new Integer[endList - startList];
+            for (int i = 0; i < array2.length; i++) {
+                for (int x = startList; x < endList; x++) {
+                    array2[i] = array[x];
+                }
+            }
+            System.out.println(Arrays.toString(array2));
+        } else {
+            System.out.println("Não há lista linear.");
+        }
     }
 
-    
+    public void showArrayList(Integer array[]) {
+        if (thereIsLinearList(array)) {
+            System.out.println(Arrays.toString(array));
+        } else {
+            System.out.println("Não há lista linear.");
+        }
+    }
+
+    private boolean thereIsLinearList(Integer array[]) {
+        for (Integer nodo : array) {
+            if (nodo != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
